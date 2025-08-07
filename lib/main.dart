46c1cc5bf80f8bccc2c47360f15945303f60bb1c
Wello <wello6668@gmail.com>
2025-08-07 23:41:05 +0300

    Initial project setup with clean folder structure

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class QuranApp extends StatelessWidget {
+  const QuranApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'القرءان العظيم',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        fontFamily: 'Roboto',
+      ),
+      home: const Placeholder(),
+    );
+  }
+}
+
+// class HomeScreen extends StatelessWidget {
+//   const HomeScreen({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(appBar: 
+//     ,);
+//   }
+// }
