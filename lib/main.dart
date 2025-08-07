d77528bde013d5417a0ff2dde6dd834784fd11bf
Wello69 <wello6668@gmail.com>
2025-08-08 00:00:03 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -16,13 +16,3 @@ class QuranApp extends StatelessWidget {
     );
   }
 }
-
-// class HomeScreen extends StatelessWidget {
-//   const HomeScreen({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(appBar: 
-//     ,);
-//   }
-// }
